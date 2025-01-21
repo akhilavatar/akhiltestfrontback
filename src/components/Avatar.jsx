7396c7ddc4c74b3f4ed1b063e3690e45f3a8f5ff
sakhilsaiteaj <sakhilsaiteaj@gmail.com>
2025-01-21 11:29:11 +0530

    calling two

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -8,10 +8,12 @@ import { useBlinking } from "../hooks/useBlinking";
 import { useLipSync } from "../hooks/useLipSync";
 import { filterEndTracks } from "../utils/animations";
 import { applyMorphTarget } from "../utils/morphTargets";
+import { SkinnedMeshes } from "./SkinnedMeshes";
+import { AVATAR_MODEL_PATH, ANIMATIONS_MODEL_PATH } from "../constants/models";
 
 export function Avatar(props) {
-  const { nodes, materials, scene } = useGLTF("/models/64f1a714fe61576b46f27ca2.glb");
-  const { animations: originalAnimations } = useGLTF("/models/animations.glb");
+  const { nodes, materials, scene } = useGLTF(AVATAR_MODEL_PATH);
+  const { animations: originalAnimations } = useGLTF(ANIMATIONS_MODEL_PATH);
   const animations = filterEndTracks(originalAnimations);
   const group = useRef();
   const { actions } = useAnimations(animations, group);
@@ -84,68 +86,7 @@ export function Avatar(props) {
       <group name="Scene">
         <group name="Armature">
           <primitive object={nodes.Hips} />
-          <skinnedMesh
-            name="EyeLeft"
-            geometry={nodes.EyeLeft.geometry}
-            material={materials.Wolf3D_Eye}
-            skeleton={nodes.EyeLeft.skeleton}
-            morphTargetDictionary={nodes.EyeLeft.morphTargetDictionary}
-            morphTargetInfluences={nodes.EyeLeft.morphTargetInfluences}
-          />
-          <skinnedMesh
-            name="EyeRight"
-            geometry={nodes.EyeRight.geometry}
-            material={materials.Wolf3D_Eye}
-            skeleton={nodes.EyeRight.skeleton}
-            morphTargetDictionary={nodes.EyeRight.morphTargetDictionary}
-            morphTargetInfluences={nodes.EyeRight.morphTargetInfluences}
-          />
-          <skinnedMesh
-            name="Wolf3D_Head"
-            geometry={nodes.Wolf3D_Head.geometry}
-            material={materials.Wolf3D_Skin}
-            skeleton={nodes.Wolf3D_Head.skeleton}
-            morphTargetDictionary={nodes.Wolf3D_Head.morphTargetDictionary}
-            morphTargetInfluences={nodes.Wolf3D_Head.morphTargetInfluences}
-          />
-          <skinnedMesh
-            name="Wolf3D_Teeth"
-            geometry={nodes.Wolf3D_Teeth.geometry}
-            material={materials.Wolf3D_Teeth}
-            skeleton={nodes.Wolf3D_Teeth.skeleton}
-            morphTargetDictionary={nodes.Wolf3D_Teeth.morphTargetDictionary}
-            morphTargetInfluences={nodes.Wolf3D_Teeth.morphTargetInfluences}
-          />
-          <skinnedMesh
-            name="Wolf3D_Body"
-            geometry={nodes.Wolf3D_Body.geometry}
-            material={materials.Wolf3D_Body}
-            skeleton={nodes.Wolf3D_Body.skeleton}
-          />
-          <skinnedMesh
-            name="Wolf3D_Outfit_Bottom"
-            geometry={nodes.Wolf3D_Outfit_Bottom.geometry}
-            material={materials.Wolf3D_Outfit_Bottom}
-            skeleton={nodes.Wolf3D_Outfit_Bottom.skeleton}
-          />
-          <skinnedMesh
-            name="Wolf3D_Outfit_Footwear"
-            geometry={nodes.Wolf3D_Outfit_Footwear.geometry}
-            material={materials.Wolf3D_Outfit_Footwear}
-            skeleton={nodes.Wolf3D_Outfit_Footwear.skeleton}
-          />
-          <skinnedMesh
-            name="Wolf3D_Outfit_Top"
-            geometry={nodes.Wolf3D_Outfit_Top.geometry}
-            material={materials.Wolf3D_Outfit_Top}
-            skeleton={nodes.Wolf3D_Outfit_Top.skeleton}
-          />
-          <skinnedMesh
-            name="Wolf3D_Hair"
-            geometry={nodes.Wolf3D_Hair.geometry}
-            material={materials.Wolf3D_Hair}
-            skeleton={nodes.Wolf3D_Hair.skeleton}
-          />
+          <SkinnedMeshes nodes={nodes} materials={materials} />
         </group>
       </group>
     </group>
---
+++ src/constants/models.js
@@ -0,0 +1,2 @@
+export const AVATAR_MODEL_PATH = "/models/64f1a714fe61576b46f27ca2.glb";
+export const ANIMATIONS_MODEL_PATH = "/models/animations.glb";
